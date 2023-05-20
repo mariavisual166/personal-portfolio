2da8f1049611af9fbcd3c0d02ec929df7c678e13
mariavisual166 <mariavisual166@gmail.com>
2023-05-20 13:34:20 +0000

    fix packege

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { NavBar } from "./components/NavBar";
+import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
